8e27d0357c2c5116d89e6caa7f9ea5d046f72ad6
Brian Durand <briandurand1990@gmail.com>
2020-04-07 11:16:15 -0300

    Agrego Meta noindex para evitar que aparesca en los buscadores

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,6 +8,7 @@ const Header = () => (
   <header className={style.header}>
     <Head>
       <title>Bibliografia - Cohorte 2020</title>
+      <meta name="robots" content="noindex" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head>
     <Link href="/">
